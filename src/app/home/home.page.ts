432a4eb257714c0849258ce1a97c70f630631af3
Vicente Garza Reyna <vgarzareyna@gmail.com>
2020-03-03 21:38:01 -0600

    Animales con sonidos!!!

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,28 +7,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  images: Array<string> = [
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg",
-    "https://definicion.mx/wp-content/uploads/2014/04/objeto.jpg"
+  animals: any = [
+    {
+      name:"Gato",
+      image:'/assets/images/cat.jpg',
+      sound:"/assets/audio/cat.mp3"
+    },
+    {
+      name:"Perro",
+      image:'/assets/images/dog.jpg',
+      sound:"/assets/audio/dog.mp3"
+    },
+    {
+      name:"Elefante",
+      image:"/assets/images/elephant.jpg",
+      sound:"/assets/audio/elephant.mp3"
+    },
+    {
+      name:"Vaca",
+      image:"/assets/images/cow.jpg",
+      sound:"/assets/audio/cow.mp3"
+    },
   ];
+
+  sonidoAnimal(animal:any) {
+    let sound = new Audio();
+    sound.src = animal.sound;
+    sound.load();
+    sound.play();
+  }
 
   constructor() {}
 
